62475707559bc3e6b9d9191b100e3856c1378358
Dinara Aliyeva <dinara.ali@gmail.com>
2018-05-03 17:15:35 +0900

    bugs to fix

--- src/P_826.java
+++ src/P_826.java
@@ -12,12 +12,20 @@ public class P_826 {
     		for(int w: worker) {
     			int profit_add = 0;
     			int diff_index = 0;
-    			System.out.println(Arrays.asList(difficulty).contains(w));
+    			List<Integer> difficulty_list = new ArrayList<Integer>();
+    			List<Integer> profit_list = new ArrayList<Integer>();
+
+    			for(int i =0; i<difficulty.length; i++) {
+        			difficulty_list.add(difficulty[i]);
+    			}
+    			for(int i =0; i<profit.length; i++) {
+    				profit_list.add(profit[i]);
+    			}
     			
-    			if(Arrays.asList(difficulty).contains(w)) {
-    				diff_index = Arrays.asList(difficulty).indexOf(w); //looked up
-    				profit_add = profit[diff_index];
-    				System.out.println("here");
+    			if(difficulty_list.contains(w)) {
+    				diff_index = difficulty_list.indexOf(w); //looked up
+    				profit_add = profit_list.get(diff_index);
+    				System.out.println("profit_add of  " + w + " " + profit_add);
     				
     			}
     			else {
@@ -26,12 +34,12 @@ public class P_826 {
     					if(diff<w) {
     						diff_arr.add(diff);
     					}}
-    				diff_index = Arrays.asList(difficulty).indexOf(Collections.min(diff_arr));
-    				System.out.println("w  " + w);
-    				System.out.println("min " + Collections.min(diff_arr));
-    				System.out.println("diff_index " + diff_index);
-    				profit_add = profit[diff_index];
-    						
+    				if(!diff_arr.isEmpty()) {
+    					diff_index = difficulty_list.indexOf(Collections.max(diff_arr));
+    					profit_add = profit_list.get(diff_index);
+    					System.out.println("profit_add of  " + w + " " + profit_add);
+    					}
+		
     					}
         		profit_sum+=profit_add;
 
@@ -47,10 +55,10 @@ public class P_826 {
 	
 	
 	public static void main(String[] args) {
-		int difficulty[] = new int[]{2,4,6,8,10};
-		int profit[] = new int[]{10,20,30,40,50};
-		int worker[] = new int[] {4,5,6,7};
-		
+		int difficulty[] = new int[]{85,47,57};
+		int profit[] = new int[]{24,66,99};
+		int worker[] = new int[] {40,25,25};
+
 		P_826 inst = new P_826();
 		inst.maxProfitAssignment(difficulty, profit, worker);
 	}
