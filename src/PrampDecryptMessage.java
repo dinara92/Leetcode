e11e444eff552978b9378ce02f8b8ce2d7204b02
Dinara Aliyeva <dinara.ali@gmail.com>
2018-08-18 17:05:35 +0900

    encrypt/decrypt

--- src/PrampDecryptMessage.java
+++ src/PrampDecryptMessage.java
@@ -1,39 +1,67 @@
 import java.io.*;
 import java.util.*;
 
-public class PrampDecryptMessage {
+class Solution {
 
-  static String decrypt(String word) {
+  static String encrypt(String word) {
     // your code goes here
     StringBuilder sb = new StringBuilder();
     
-    System.out.println(word.charAt(0));
-    
-    char f1 = (char)(word.charAt(0)+1);
-    sb.append(f1);
-    
-    for(int i=1;i<word.length();i++){
-    	char newc;
-        if(i==1)
-          newc = (char)(word.charAt(i) + f1);
-        else {
-          newc = (char)(word.charAt(i) + word.charAt(i-1));
-        }
-        int newint = newc;
 
-        while(newint<97 || newint>122){
-          newint = newint - 26;
-        }
-        System.out.println(newint);
-        sb.append((char)newint);
-    }
+    int newint = 0;
+    int prev = 1;
     
+    for(int i=0;i<word.length();i++){
+        newint = word.charAt(i);
+      
+       newint=newint+prev;
+
+       while(newint>122){ //<97 other way around
+         newint-=26;
+       }
+       System.out.println(newint);
+       prev=newint;
+
+      
+       sb.append((char)newint);
+      
+
+    }
+
     System.out.println(sb.toString());
     return sb.toString();
   }
 
+  static String decrypt(String word) {
+    // your code goes here
+    StringBuilder sb = new StringBuilder();
+
+    int newint = 0;
+    int prev = 1; //for first letter
+    
+    for(int i=0;i<word.length();i++){
+       
+      newint = word.charAt(i);
+      newint = newint - prev;
+
+       while(newint<97){ 
+         newint+=26;
+       }
+
+      prev += newint;
+
+      System.out.println(prev);
+      sb.append((char)newint);
+    }
+
+    System.out.println(sb.toString());
+    return sb.toString();
+  }
+  
   public static void main(String[] args) {
-      String decrypted = decrypt("crime");
+      String encrypted = encrypt("crime");
+      //String decrypted = decrypt("dnotq");
+
   }
 
 }
